b2f438d563a92a99fc70e4938ee3f197a86324b8
GahwonLee <creativitRy@gmail.com>
2018-10-20 12:16:42 -0500

    Initial commit

---
+++ tap-n-nap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tap_n_nap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
